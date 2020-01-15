1ad520680d345044ecc8c080c1ad0b754a071569
Erik Sharp <mr.erik.sharp@gmail.com>
2020-01-15 18:16:43 +0100

    adding routing and the dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,18 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { HeroesComponent } from "./heroes/heroes.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 
-const routes: Routes = [];
+//Routes tell the Router which view to display when a user clicks a link or pastes a URL into the browser address bar
+const routes: Routes = [
+    //This route redirects a URL that fully matches the empty path to the route whose path is '/dashboard'
+    { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+    { path: "heroes", component: HeroesComponent },
+    { path: "dashboard", component: DashboardComponent }
+];
 
 @NgModule({
+    //The following line adds the RouterModule to the AppRoutingModule imports array and configures it with the routes in one step
     imports: [RouterModule.forRoot(routes)],
     exports: [RouterModule]
 })
